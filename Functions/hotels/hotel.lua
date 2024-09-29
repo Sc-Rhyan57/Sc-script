e9cc9759155c46fb3673d4cf0c36de64c204a498
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-09-29 12:52:51 -0300

    Update hotel.lua

--- Functions/hotels/hotel.lua
+++ Functions/hotels/hotel.lua
@@ -340,6 +340,88 @@ local function verificarNovoLoot()
 end
 -- DOORS ESP
 -- ESP para portas
+-- Função para arredondar números
+function round(number, decimals)
+    local power = 10 ^ decimals
+    return math.floor(number * power) / power
+end
+
+
+local ESPColors = {
+    Door = Color3.fromRGB(241, 196, 15),
+}
+
+local function setupESPForDoors(door)
+    local highlight = ESPLibrary.ESP.Highlight({
+        Name = "Door",
+        Model = door,
+        FillColor = ESPColors.Door,
+        OutlineColor = ESPColors.Door,
+        TextColor = ESPColors.Door,
+
+        Tracer = {
+            Enabled = true,
+            Color = ESPColors.Door
+        }
+    })
+end
+
+spawn(function()
+    while wait(0.2) do 
+        if doorESPEnabled then
+            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                if room:FindFirstChild("Door") and room.Door:FindFirstChild("Door") then
+                    local door = room.Door.Door
+
+                    if not door:FindFirstChild("Highlight") then
+                        setupESPForDoors(door)
+                    end
+
+                    local bb = door:FindFirstChild("BillBoard")
+                    if not bb then
+                        bb = Instance.new('BillboardGui', door)
+                        bb.Adornee = door
+                        bb.ExtentsOffset = Vector3.new(0, 1, 0)
+                        bb.AlwaysOnTop = true
+                        bb.Size = UDim2.new(0, 6, 0, 6)
+                        bb.StudsOffset = Vector3.new(0, 1, 0)
+                        bb.Name = "BillBoard"
+
+                        local txtlbl = Instance.new('TextLabel', bb)
+                        txtlbl.ZIndex = 10
+                        txtlbl.BackgroundTransparency = 1
+                        txtlbl.Position = UDim2.new(0, 0, 0, -45)
+                        txtlbl.Size = UDim2.new(1, 0, 10, 0)
+                        txtlbl.Font = Enum.Font.ArialBold
+                        txtlbl.TextSize = 12
+                        txtlbl.Text = "Door " .. room.Name
+                        txtlbl.TextStrokeTransparency = 0.5
+                        txtlbl.TextColor3 = ESPColors.Door
+
+                        local txtlbl2 = Instance.new('TextLabel', bb)
+                        txtlbl2.ZIndex = 10
+                        txtlbl2.BackgroundTransparency = 1
+                        txtlbl2.Position = UDim2.new(0, 0, 0, -15)
+                        txtlbl2.Size = UDim2.new(1, 0, 10, 0)
+                        txtlbl2.Font = Enum.Font.ArialBold
+                        txtlbl2.TextSize = 12
+                        txtlbl2.Text = "? Studs"
+                        txtlbl2.Name = "Dist"
+                        txtlbl2.TextStrokeTransparency = 0.5
+                        txtlbl2.TextColor3 = ESPColors.Door
+                    end
+
+                    local distance = (game.Players.LocalPlayer.Character.PrimaryPart.Position - door.Position).Magnitude
+                    bb.Dist.Text = round(distance, 1) .. " Studs"
+                end
+            end
+        end
+    end
+end)
+
+
+ESPLibrary.Rainbow.Set(true)
+
 -- New Esp Sicripti
 local doorESPEnabled = false
 local activeDoors = {}
@@ -463,12 +545,20 @@ local VisualsEsp = Window:MakeTab({
 VisualsEsp:AddToggle({
     Name = "ESP de Portas",
     Default = false,
-    Callback = function(valor)
-        doorESPEnabled = valor
-        if doorESPEnabled then
-            atualizarESPPortas()
-        else
-            desativarESPPortas()
+    Callback = function(value)
+        doorESPEnabled = value
+        if not doorESPEnabled then
+            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                if room:FindFirstChild("Door") and room.Door:FindFirstChild("Door") then
+                    local door = room.Door.Door
+                    if door:FindFirstChild("Highlight") then
+                        door.Highlight:Destroy()
+                    end
+                    if door:FindFirstChild("BillBoard") then
+                        door.BillBoard:Destroy()
+                    end
+                end
+            end
         end
     end
 })
