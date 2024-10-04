8fbc22e28de10fa255b0171020b5dd982fd185c1
Sc-Rhyan57 <rhyanlus501@gmail.com>
2024-10-04 16:32:17 -0300

    Create lobby.lua

---
+++ Functions/hotels/lobby.lua
@@ -0,0 +1,145 @@
+local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+local Window = OrionLib:MakeWindow({IntroText = "Seeker Hub × Paint", Name = "💎 RSeeker(Lobby)", HidePremium = false, SaveConfig = true, ConfigFolder = ".seekerLobby"})
+
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://4590657171"
+sound.Volume = 1
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "🔔 Notificação | Rseeker System",
+    Text = "⚙️ Lobby Menu Carregado com Sucesso!",
+    Icon = "rbxassetid://76411928845479",
+    Duration = 5
+})
+
+local Script = {
+    CurrentBadge = 0,
+    Achievements = {
+        "SurviveWithoutHiding",
+        "SurviveGloombats",
+        "SurviveSeekMinesSecond",
+        "TowerHeroesGoblino",
+        "EscapeBackdoor",
+        "SurviveFiredamp",
+        "CrucifixDread",
+        "EnterRooms",
+        "EncounterVoid",
+        "Join",
+        "DeathAmt100",
+        "UseCrucifix",
+        "EncounterSpider",
+        "SurviveHalt",
+        "SurviveRush",
+        "DeathAmt10",
+        "Revive",
+        "PlayFriend",
+        "SurviveNest",
+        "CrucifixFigure",
+        "CrucifixAmbush",
+        "PlayerBetrayal",
+        "SurviveEyes",
+        "KickGiggle",
+        "EscapeMines",
+        "GlowstickGiggle",
+        "DeathAmt1",
+        "SurviveSeek",
+        "UseRiftMutate",
+        "CrucifixGloombatSwarm",
+        "SurviveScreech",
+        "SurviveDread",
+        "SurviveSeekMinesFirst",
+        "CrucifixHalt",
+        "TowerHeroesVoid",
+        "JoinLSplash",
+        "CrucifixDupe",
+        "EncounterGlitch",
+        "JeffShop",
+        "CrucifixScreech",
+        "SurviveGiggle",
+        "EscapeHotelMod1",
+        "SurviveDupe",
+        "CrucifixRush",
+        "EscapeBackdoorHunt",
+        "EscapeHotel",
+        "CrucifixGiggle",
+        "EscapeFools",
+        "UseRift",
+        "SpecialQATester",
+        "EscapeRetro",
+        "TowerHeroesHard",
+        "EnterBackdoor",
+        "EscapeRooms1000",
+        "EscapeRooms",
+        "EscapeHotelMod2",
+        "EncounterMobble",
+        "CrucifixGrumble",
+        "UseHerbGreen",
+        "CrucifixSeek",
+        "JeffTipFull",
+        "SurviveFigureLibrary",
+        "TowerHeroesHotel",
+        "CrucifixEyes",
+        "BreakerSpeedrun",
+        "SurviveAmbush",
+        "SurviveHide",
+        "JoinAgain"
+    }
+}
+
+local function LoopAchievements()
+    task.spawn(function()
+        while OrionLib.Flags["LoopAchievements"].Value do
+            if Script.CurrentBadge >= #Script.Achievements then 
+                Script.CurrentBadge = 0 
+            end
+            
+            Script.CurrentBadge = Script.CurrentBadge + 1
+            local randomAchievement = Script.Achievements[Script.CurrentBadge]
+            remotesFolder.FlexAchievement:FireServer(randomAchievement)
+
+            task.wait(OrionLib.Flags["LoopAchievementsSpeed"].Value)
+        end
+    end)
+end
+
+OrionLib:MakeWindow({
+    Name = "Achievements Control",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "AchievementsConfig"
+})
+
+local AchievementTab = OrionLib:MakeTab({
+    Name = "Achievements",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+AchievementTab:AddToggle({
+    Name = "Loop Achievements",
+    Default = false,
+    Flag = "LoopAchievements",
+    Callback = function(Value)
+        if Value then
+            LoopAchievements()
+        end
+    end
+})
+
+AchievementTab:AddSlider({
+    Name = "Loop Speed",
+    Min = 0.05,
+    Max = 1,
+    Default = 0.1,
+    Color = Color3.fromRGB(255, 0, 0),
+    Increment = 0.01,
+    Flag = "LoopAchievementsSpeed",
+    Callback = function(Value)
+    end
+})
+
+OrionLib:Init()
