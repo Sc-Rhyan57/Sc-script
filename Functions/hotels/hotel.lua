45dc419f75948de41491c379753ffee8dd6be9ad
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-06-07 23:53:26 -0300

    Update hotel.lua

--- Functions/hotels/hotel.lua
+++ Functions/hotels/hotel.lua
@@ -1,4 +1,5 @@
-local OrionLib = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({IntroText = "RSeekerHUb 1.0.0", Name = "👁️ | RSeeKer Hub", HidePremium = false, SaveConfig = true, ConfigFolder = ".seeker"})
 
 local sound = Instance.new("Sound")
